afe8efc008f6fdcd57bdda48d61c65b244d93d13
Arkadiusz Gil <arkadiusz.gil@erlang-solutions.com>
2017-01-13 23:20:20 +0100

    Add YAML mode

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (markdown-mode neotree erlang elixir-mode smartparens highlight-parentheses))))
+    (yaml-mode markdown-mode neotree erlang elixir-mode smartparens highlight-parentheses))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
