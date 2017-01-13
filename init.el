08a0113a6c8251181e076dd0fe87dbd77fb460e3
Arkadiusz Gil <arkadiusz.gil@erlang-solutions.com>
2017-01-13 22:16:36 +0100

    Add markdown mode

--- init.el
+++ init.el
@@ -12,14 +12,17 @@
 ;; use spaces for tabs
 (setq-default indent-tabs-mode nil)
 
+;; display trailing whitespace
+(setq-default show-trailing-whitespace t)
+
 (custom-set-variables
- ;; custom-set-variables was added by Custom.  If you edit it by hand,
- ;; you could mess it up, so be careful.  Your init file should
- ;; contain only one such instance.  If there is more than one, they
- ;; won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (neotree erlang elixir-mode smartparens highlight-parentheses))))
+    (markdown-mode neotree erlang elixir-mode smartparens highlight-parentheses))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
